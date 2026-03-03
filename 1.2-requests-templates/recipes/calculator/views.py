4c71ef7e2198b0b5fe6ebb8d9d06eb0a655e0765
Artem Osipenko <Jonyxt@gmail.com>
2026-03-03 11:17:58 +0700

    Added HW 1.2

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,16 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def recipe(request):
+    dish = request.GET.get('dish', 'omlet')
+    servings = int(request.GET.get('servings', 1))
+    recipe_for_servings = {
+        key: round(value * servings, 2) for key, value in DATA[dish].items()
+    }
+    context = {
+        'recipe': recipe_for_servings,
+    }
+    return render(request, 'calculator/index.html', context)
+
